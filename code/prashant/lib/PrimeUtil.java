5dfea2a0429f1a512adfe332221bb55966d3b6e0
prashant <prashant@prashant.(none)>
2011-09-12 21:03:56 +0530

    delegated utils to PrimeUtil

--- code/prashant/lib/PrimeUtil.java
+++ code/prashant/lib/PrimeUtil.java
@@ -1,6 +1,7 @@
 package prashant.lib;
 
-import java.util.Vector;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Utility class for Prime numbers
@@ -8,20 +9,28 @@ import java.util.Vector;
  * @author prashant
  */
 public class PrimeUtil {
+	
 	/**
-	 * Generate Unique prime factors.
+	 * Generate Unique primeFactors
 	 * 
 	 * @param num
-	 * @return Vector of unique prime factors
+	 * @return empty if no is prime.
 	 */
-	public Vector<Integer> primeFactors(int num) {
-		Vector<Integer> t = new Vector<Integer>();
+	public static Set<Integer> primeFactors(int num) {
+
+		Set<Integer> t = new TreeSet<Integer>();
+		if (num < 2) {
+			// t.add(2);
+			return t;
+		}
 		for (int i = 2; i <= Math.sqrt(num); i++) {
 			if (chkPrime(i)) {
 				if (num % i == 0) {
 					t.add(i);
 					if ((i != num / i) && chkPrime(num / i))
 						t.add(num / i);
+					else
+						t.addAll(primeFactors(num / i));
 				}
 			}
 		}
@@ -32,9 +41,10 @@ public class PrimeUtil {
 	 * Deterministic Prime testing.
 	 * 
 	 * @param num
-	 * @return wether true or false
+	 * @return
 	 */
-	public boolean chkPrime(int num) {
+	public static boolean chkPrime(int num) {
+		if (num==1) return false;
 		if (num == 2 || num == 3)
 			return true;
 		if (num % 2 == 0)
@@ -42,11 +52,10 @@ public class PrimeUtil {
 		else if (num % 3 == 0)
 			return false;
 
-		for (int i = 5; i <= Math.sqrt(num); i += 2) {
+		for (int i = 5; i * i <= num; i += 2) {
 			if (num % i == 0)
 				return false;
 		}
 		return true;
 	}
-
 }
--- code/prashant/tc/SRM517/CompositeSmash.java
+++ code/prashant/tc/SRM517/CompositeSmash.java
@@ -1,7 +1,7 @@
 package prashant.tc.SRM517;
 
 import java.util.Set;
-import java.util.TreeSet;
+import prashant.lib.PrimeUtil;
 
 public class CompositeSmash {
 
@@ -20,11 +20,11 @@ public class CompositeSmash {
 		if (N % target == 0) {
 			if (N == target) {
 				temp = new String("Yes");
-			} else if (chkPrime(target)) {
+			} else if (PrimeUtil.chkPrime(target)) {
 				temp = new String("Yes");
 			} else {
 				int p=(int) Math.ceil(Math.sqrt(target));
-				if(chkPrime(p)&& p*p==target && N>p*p ){
+				if(PrimeUtil.chkPrime(p)&& p*p==target && N>p*p ){
 					while(N>1)N/=p;
 					if(N==1){
 						temp=new String("Yes");
@@ -34,52 +34,5 @@ public class CompositeSmash {
 		}
 			return temp;
 	}
-	
-	/**
-	 * Generate Unique primeFactors
-	 * 
-	 * @param num
-	 * @return
-	 */
-	public Set<Integer> primeFactors(int num) {
 
-		Set<Integer> t = new TreeSet<Integer>();
-		if (num < 2) {
-			// t.add(2);
-			return t;
-		}
-		for (int i = 2; i <= Math.sqrt(num); i++) {
-			if (chkPrime(i)) {
-				if (num % i == 0) {
-					t.add(i);
-					if ((i != num / i) && chkPrime(num / i))
-						t.add(num / i);
-					else
-						t.addAll(primeFactors(num / i));
-				}
-			}
-		}
-		return t;
-	}
-
-	/**
-	 * Deterministic Prime testing.
-	 * 
-	 * @param num
-	 * @return
-	 */
-	public boolean chkPrime(int num) {
-		if (num == 2 || num == 3)
-			return true;
-		if (num % 2 == 0)
-			return false;
-		else if (num % 3 == 0)
-			return false;
-
-		for (int i = 5; i * i <= num; i += 2) {
-			if (num % i == 0)
-				return false;
-		}
-		return true;
-	}
 }

8cfa4d25cdfda7f5073b6942a951ad80ae321f43
Prashant <prashant.iiith@gmail.com>
2011-09-14 21:32:39 +0530

    Modified

--- code/prashant/tc/SRM518/SRM518.java
+++ code/prashant/tc/SRM518/SRM518.java
@@ -6,13 +6,12 @@ public class SRM518 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		TwiceString t= new TwiceString();
 		System.out.println(t.getShortest("aba"));
 		System.out.println(t.getShortest("xxxxx"));
 		System.out.println(t.getShortest("topcoder"));
 		System.out.println(t.getShortest("abracadabra"));
-		System.out.println(t.getShortest("abracabrababra").length());
+		System.out.println(t.getShortest("abracabrababra"));
 		LargestSubsequence n = new LargestSubsequence();
 		System.out.println(n.getLargest("test"));
 		System.out.println(n.getLargest("ksljdhfakjsdfhakjsdhfkjlashdfhalskjdfhas dfkjahsdfsfasdt"));

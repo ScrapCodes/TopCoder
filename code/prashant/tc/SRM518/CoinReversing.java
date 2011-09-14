1ad84772aaeed6dbc5568167072e3e92a47d5c29
Prashant <prashant.iiith@gmail.com>
2011-09-14 21:29:57 +0530

    refrected unused code.

--- code/prashant/tc/SRM518/CoinReversing.java
+++ code/prashant/tc/SRM518/CoinReversing.java
@@ -1,9 +1,10 @@
 package prashant.tc.SRM518;
 
 public class CoinReversing {
-	public double expectedHeads(int N, int[] a){
+	public double expectedHeads(int N, int[] a) {
+
 		
 		return N;
-		
+
 	}
 }
--- code/prashant/tc/SRM518/LargestSubsequence.java
+++ code/prashant/tc/SRM518/LargestSubsequence.java
@@ -14,8 +14,6 @@ public class LargestSubsequence {
 			int k=0;
 			for(int i=(s.length()-1);i>=0;i--){
 				
-//				     l1.append(temp[i]);
-//				     x.add(l1.toString());
 				boolean flag=false;
 				for(int j=k;j<s.length();j++)
 					if(s.charAt(j)==temp[i]){
@@ -27,11 +25,7 @@ public class LargestSubsequence {
 				l1.append(temp[i]);
 				
 			}
-			String[] l2=new String[1000];
-			l2=x.toArray(new String[0]);
-			Arrays.sort(l2);
 			
-//			return l2[l2.length-1];
 			return l1.toString();
 			
 		}
